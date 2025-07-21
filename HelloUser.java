6411278bd32a2a9c61e3447ac465b50745f5e231
SvetlanaKup <svetakupriyanova@mail.ru>
2025-07-21 20:53:49 +0300

    Add files via upload

---
+++ HelloUser.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите ваше имя;");
+        String name = sc.nextLine();
+        System.out.println("Привет, " + name + "!");
+
+    }
+}
